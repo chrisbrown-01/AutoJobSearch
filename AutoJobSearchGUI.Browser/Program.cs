e9473e1b3eada4a6293dda69edd5b71f34cd9d77
Chris Brown <43240184+chrisbrown-01@users.noreply.github.com>
2023-10-04 20:45:26 -0400

    create new gui files

---
+++ AutoJobSearchGUI.Browser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Versioning;
+using System.Threading.Tasks;
+
+using AutoJobSearchGUI;
+
+using Avalonia;
+using Avalonia.Browser;
+
+[assembly: SupportedOSPlatform("browser")]
+
+internal partial class Program
+{
+    private static async Task Main(string[] args) => await BuildAvaloniaApp()
+            .WithInterFont()
+            .StartBrowserAppAsync("out");
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>();
+}
---
+++ AutoJobSearchGUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AutoJobSearchGUI.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
