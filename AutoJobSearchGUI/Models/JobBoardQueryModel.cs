43b5ced3817e1490414d6f384d9a5e4b506c7a31
Chris Brown <43240184+chrisbrown-01@users.noreply.github.com>
2023-10-07 19:41:58 -0400

    finalize all fields for advanced query model

--- AutoJobSearchGUI/Models/JobBoardQueryModel.cs
+++ AutoJobSearchGUI/Models/JobBoardQueryModel.cs
@@ -25,15 +25,63 @@ namespace AutoJobSearchGUI.Models
         private bool _isHidden;
 
         [ObservableProperty]
-        private bool _orderById;
+        private bool _sortById;
 
         [ObservableProperty]
-        private bool _orderBySearchTerm;
+        private bool _sortBySearchTerm;
 
         [ObservableProperty]
-        private bool _orderByCreatedAt;
+        private bool _sortByCreatedAt;
 
         [ObservableProperty]
-        private bool _orderByScore;
+        private bool _sortByScore;
+
+        [ObservableProperty]
+        private bool _orderByAscending;
+
+        [ObservableProperty]
+        private bool _orderByDescending;
+
+        [ObservableProperty]
+        private DateTimeOffset _searchedOnDate = DateTime.Today;
+
+        [ObservableProperty]
+        private bool _searchedOnDateEnabled;
+
+        [ObservableProperty]
+        private bool _searchedBetweenDatesEnabled;
+
+        [ObservableProperty]
+        private DateTimeOffset _searchedOnDateStart = DateTime.Today.AddDays(-1);
+
+        [ObservableProperty]
+        private DateTimeOffset _searchedOnDateEnd = DateTime.Today;
+
+        [ObservableProperty]
+        private bool _scoreEqualsEnabled;
+
+        [ObservableProperty]
+        private bool _scoreRangeEnabled;
+
+        [ObservableProperty]
+        private int _scoreEquals = 0;
+
+        [ObservableProperty]
+        private int _scoreRangeMin = -1;
+
+        [ObservableProperty]
+        private int _scoreRangeMax = 1;
+
+        [ObservableProperty]
+        private bool _searchTermQueryStringEnabled;
+
+        [ObservableProperty]
+        private string _searchTermQueryString = string.Empty;
+
+        [ObservableProperty]
+        private bool _jobDescriptionQueryStringEnabled;
+
+        [ObservableProperty]
+        private string _jobDescriptionQueryString = string.Empty;
     }
 }
