64e2a02406cd5493782d45873c37ca84140a19c3
Chris Brown <43240184+chrisbrown-01@users.noreply.github.com>
2023-10-09 11:32:06 -0400

    create models for jobsearch view

---
+++ AutoJobSearchGUI/Models/JobSearchProfileModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoJobSearchGUI.Models
+{
+    public partial class JobSearchProfileModel : ObservableObject
+    {
+        public int Id { get; }
+
+        [ObservableProperty]
+        private string _profileName = string.Empty;
+
+        [ObservableProperty]
+        private string _searches = string.Empty;
+
+        [ObservableProperty]
+        private string _keywordsPositive = string.Empty;
+
+        [ObservableProperty]
+        private string _keywordsNegative = string.Empty;
+
+        [ObservableProperty]
+        private string _sentimentsPositive = string.Empty;
+
+        [ObservableProperty]
+        private string _sentimentsNegative = string.Empty;
+    }
+}
--- AutoJobSearchGUI/ViewModels/JobSearchViewModel.cs
+++ AutoJobSearchGUI/ViewModels/JobSearchViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using AutoJobSearchGUI.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +12,21 @@ namespace AutoJobSearchGUI.ViewModels
 {
     public partial class JobSearchViewModel : ViewModelBase // Needs to be public for View previewer to work
     {
-        public string Greeting => "Job search";
+        [ObservableProperty]
+        private List<JobSearchProfileModel> _searchProfiles;
+
+        [ObservableProperty]
+        private JobSearchProfileModel? _selectedSearchProfile;
+
+        public JobSearchViewModel()
+        {
+            SearchProfiles = new List<JobSearchProfileModel>();
+        }
+
+        //partial void OnSelectedComboBoxItemChanged(string? value)
+        //{
+        //    if (value.IsNullOrEmpty()) return;
+        //    Debug.WriteLine($"selected value is {value}");
+        //}
     }
 }
