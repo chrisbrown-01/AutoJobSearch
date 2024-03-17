e810a70dfe2ebd30b7562748d90066b54aeef56f
chrisbrown-01 <chrisbrown1@hotmail.ca>
2024-03-17 14:27:04 -0400

    add joblistingviewmodel and contactshelpers tests

--- AutoJobSearchGUI.Tests/ViewModels/JobListingViewModel_Tests.cs
+++ AutoJobSearchGUI.Tests/ViewModels/JobListingViewModel_Tests.cs
@@ -1,17 +1,22 @@
 ﻿using AutoFixture;
 using AutoFixture.AutoNSubstitute;
 using AutoJobSearchGUI.Data;
+using AutoJobSearchGUI.Helpers;
 using AutoJobSearchGUI.Models;
+using AutoJobSearchGUI.Services;
 using AutoJobSearchGUI.ViewModels;
+using AutoJobSearchShared.Enums;
 using AutoJobSearchShared.Models;
 using FluentAssertions;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 
 namespace AutoJobSearchGUI.Tests.ViewModels
 {
     public class JobListingViewModel_Tests
     {
         private readonly IDbContext _dbContext;
+        private readonly IFilesService _filesService;
         private readonly IFixture _fixture;
         private readonly JobListingViewModel _viewModel;
 
@@ -19,6 +24,7 @@ namespace AutoJobSearchGUI.Tests.ViewModels
         {
             _fixture = new Fixture().Customize(new AutoNSubstituteCustomization());
             _dbContext = Substitute.For<IDbContext>();
+            _filesService = Substitute.For<IFilesService>();
             _viewModel = new JobListingViewModel(_dbContext);
         }
 
@@ -73,6 +79,178 @@ namespace AutoJobSearchGUI.Tests.ViewModels
             _viewModel.JobListing.Should().BeEquivalentTo(jobListingWithDetails);
             _viewModel.JobListing.DetailsPopulated.Should().BeTrue();
             _viewModel.JobListing.EnableEvents.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ViewContact_ValidContactId_InvokesChangeViewEvent()
+        {
+            // Arrange
+            _viewModel.SelectedContactId = Math.Abs(_fixture.Create<int>());
+            bool wasCalled = false;
+            _viewModel.ChangeViewToContactRequest += (id) => wasCalled = true;
+
+            // Act
+            _viewModel.ViewContactCommand.Execute(null);
+
+            // Assert
+            _viewModel.JobListing.EnableEvents.Should().Be(false);
+            wasCalled.Should().Be(true);
+        }
+
+        [Fact]
+        public void ViewContact_InvalidContactId_DoesNotInvokeChangeViewEvent()
+        {
+            // Arrange
+            _viewModel.SelectedContactId = 0;
+            bool wasCalled = false;
+            _viewModel.ChangeViewToContactRequest += (id) => wasCalled = true;
+
+            // Act
+            _viewModel.ViewContactCommand.Execute(null);
+
+            // Assert
+            _viewModel.JobListing.EnableEvents.Should().Be(false);
+            wasCalled.Should().Be(false);
+        }
+
+        [Fact]
+        public void AddAssociatedContact_InvokesEvent()
+        {
+            // Arrange
+            _viewModel.JobListing = _fixture.Create<JobListingModel>();
+            bool wasCalled = false;
+            _viewModel.CreateNewContactWithAssociatedJobIdRequest += (id) => wasCalled = true;
+
+            // Act
+            _viewModel.AddAssociatedContactCommand.Execute(null);
+
+            // Assert
+            _viewModel.JobListing.EnableEvents.Should().Be(false);
+            wasCalled.Should().Be(true);
+        }
+
+        [Fact]
+        public void ToggleEditMode_Inverts_IsEditModeEnabled_Property()
+        {
+            // Arrange
+            _viewModel.IsEditModeEnabled = _fixture.Create<bool>();
+            var initialIsEditModeEnabled = _viewModel.IsEditModeEnabled;
+
+            if (initialIsEditModeEnabled)
+            {
+                _viewModel.EditButtonFontWeight = "ExtraBold";
+            }
+            else
+            {
+                _viewModel.EditButtonFontWeight = "Regular";
+            }
+
+            var initialEditButtonFontWeight = _viewModel.EditButtonFontWeight;
+
+            // Act
+            _viewModel.ToggleEditModeCommand.Execute(null);
+
+            // Assert
+            _viewModel.IsEditModeEnabled.Should().Be(!initialIsEditModeEnabled);
+            _viewModel.EditButtonFontWeight.Should().NotBe(initialEditButtonFontWeight);
+        }
+
+
+        [Fact]
+        public async void CreateJobAsync_CreatesNewJob()
+        {
+            // Arrange
+            _viewModel.JobListing = _fixture.Create<JobListingModel>();
+
+            var newJob = _fixture.Create<JobListing>();
+            var newJobListingModel = JobListingHelpers.ConvertJobListingToJobListingModel(newJob);
+
+            Singletons.JobListings = _fixture.CreateMany<JobListingModel>().ToList();
+            var initialJobListingsCount = Singletons.JobListings.Count;
+
+            Singletons.Contacts = _fixture.CreateMany<ContactModel>().ToList();
+
+            bool wasCalled = false;
+            _viewModel.UpdateJobBoardViewRequest += () => wasCalled = true;
+
+            _dbContext.CreateJobAsync().Returns(newJob);
+            _dbContext.GetJobListingDetailsByIdAsync(newJob.Id).Returns(newJob);
+
+            // Act
+            await _viewModel.CreateJobCommand.ExecuteAsync(null);
+
+            // Assert
+            wasCalled.Should().Be(true);
+            await _dbContext.Received().CreateJobAsync();
+            Singletons.JobListings.Count.Should().Be(initialJobListingsCount + 1);
+        }
+
+        [Fact]
+        public async void GoToPreviousJobAsync_GoesToPreviousJob_WhenValid()
+        {
+            // Arrange
+            Singletons.JobListings = _fixture.CreateMany<JobListingModel>().ToList();
+            Singletons.Contacts = _fixture.CreateMany<ContactModel>().ToList();
+            _viewModel.JobListing = Singletons.JobListings.Last();
+            var initialJobListing = _viewModel.JobListing;
+
+            var newJob = _fixture.Create<JobListing>();
+            _dbContext.GetJobListingDetailsByIdAsync(Arg.Any<int>()).Returns(newJob);
+
+            // Act
+            await _viewModel.GoToPreviousJobCommand.ExecuteAsync(null);
+
+            // Assert
+            _viewModel.JobListing.Should().NotBe(initialJobListing);
+        }
+
+        [Fact]
+        public async void GoToPreviousJobAsync_DoesNotGoToPreviousJob_WhenInvalid()
+        {
+            // Arrange
+            Singletons.JobListings = _fixture.CreateMany<JobListingModel>().ToList();
+            _viewModel.JobListing = Singletons.JobListings.First();
+            var initialJobListing = _viewModel.JobListing;
+
+            // Act
+            await _viewModel.GoToPreviousJobCommand.ExecuteAsync(null);
+
+            // Assert
+            _viewModel.JobListing.Should().Be(initialJobListing);
+        }
+
+        [Fact]
+        public async void GoToNextJobAsync_GoesToNextJob_WhenValid()
+        {
+            // Arrange
+            Singletons.JobListings = _fixture.CreateMany<JobListingModel>().ToList();
+            Singletons.Contacts = _fixture.CreateMany<ContactModel>().ToList();
+            _viewModel.JobListing = Singletons.JobListings.First();
+            var initialJobListing = _viewModel.JobListing;
+
+            var newJob = _fixture.Create<JobListing>();
+            _dbContext.GetJobListingDetailsByIdAsync(Arg.Any<int>()).Returns(newJob);
+
+            // Act
+            await _viewModel.GoToNextJobCommand.ExecuteAsync(null);
+
+            // Assert
+            _viewModel.JobListing.Should().NotBe(initialJobListing);
+        }
+
+        [Fact]
+        public async void GoToNextJobAsync_DoesNotGoToNextJob_WhenInvalid()
+        {
+            // Arrange
+            Singletons.JobListings = _fixture.CreateMany<JobListingModel>().ToList();
+            _viewModel.JobListing = Singletons.JobListings.Last();
+            var initialJobListing = _viewModel.JobListing;
+
+            // Act
+            await _viewModel.GoToNextJobCommand.ExecuteAsync(null);
+
+            // Assert
+            _viewModel.JobListing.Should().Be(initialJobListing);
         }
     }
 }
