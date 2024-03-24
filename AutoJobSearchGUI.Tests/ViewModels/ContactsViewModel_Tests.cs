8139d80d56ef7bfbf7c523cb9f207da4a32dff1a
chrisbrown-01 <chrisbrown1@hotmail.ca>
2024-03-23 20:05:28 -0400

    add tests for contactsviewmodel

--- AutoJobSearchGUI.Tests/ViewModels/ContactsViewModel_Tests.cs
+++ AutoJobSearchGUI.Tests/ViewModels/ContactsViewModel_Tests.cs
@@ -3,6 +3,8 @@ using AutoFixture.AutoNSubstitute;
 using AutoJobSearchGUI.Data;
 using AutoJobSearchGUI.Models;
 using AutoJobSearchGUI.ViewModels;
+using AutoJobSearchShared.Models;
+using FluentAssertions;
 using NSubstitute;
 using System;
 using System.Collections.Generic;
@@ -27,6 +29,37 @@ namespace AutoJobSearchGUI.Tests.ViewModels
             _viewModel = new ContactsViewModel(_dbContext);
             _singletonJobListings = _fixture.CreateMany<JobListingModel>().ToList();
             _singletonContacts = _fixture.CreateMany<ContactModel>().ToList();
+        }
+
+        [Fact]
+        public async Task ExecuteQuery_UpdatesPropertiesCorrectly()
+        {
+            // Arrange
+            var pageSize = _viewModel.PageSize;
+            var contacts = _fixture.CreateMany<Contact>(10000);
+            var contactsAssociatedJobIds = _fixture.CreateMany<ContactAssociatedJobId>(1000);
+
+            _dbContext.GetAllContactsAsync().Returns(contacts);
+            _dbContext.GetAllContactsAssociatedJobIdsAsync().Returns(contactsAssociatedJobIds);
+
+            _viewModel.ContactsQueryModel = new ContactsQueryModel();
+
+            // Act
+            await _viewModel.ExecuteQueryCommand.ExecuteAsync(null);
+
+            // Assert
+            await _dbContext.Received().GetAllContactsAsync();
+            await _dbContext.Received().GetAllContactsAssociatedJobIdsAsync();
+
+            _viewModel.PageIndex.Should().Be(0);
+            _viewModel.ContactsDisplayed.Should().NotBeNull();
+            _viewModel.ContactsDisplayed.Should().BeOfType<List<ContactModel>>();
+            _viewModel.ContactsDisplayed.Count.Should().BeLessThanOrEqualTo(_viewModel.PageSize);
+
+            if (_viewModel.ContactsDisplayed.Any())
+            {
+                _viewModel.ContactsDisplayed.Should().AllSatisfy(x => x.EnableEvents.Should().BeFalse());
+            }
         }
 
         // Arrange
